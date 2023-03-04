c0d98f79428c9d3d0874184c32f0fc413a540c19
EgorSavage <119173955+EgorSavage@users.noreply.github.com>
2023-03-05 00:25:05 +0100

    add burger and its animation

--- src/components/Nav/NavBar.jsx
+++ src/components/Nav/NavBar.jsx
@@ -9,7 +9,15 @@ import { useState } from "react"
 
 const NavBar = () => {
 
-   
+   const [burgerActive, setBurgerActive] = useState (false);
+
+   const getBurgerActive = () => {
+    if(burgerActive === false) {
+        setBurgerActive(true)
+    } else {
+        setBurgerActive(false)
+    }
+   }
 
     return ( 
         <header className="navbar_container">
@@ -44,6 +52,43 @@ const NavBar = () => {
                         <div className="cart_itmes">3</div>
                     </li>
                 </ul>
+                <div className="tablet_navbar">
+                    <button onClick={getBurgerActive} className="tablet_burger--">
+                        <div className={`burger_line1--${burgerActive ? "active" : ""}`}></div>
+                        <div className={`burger_line2--${burgerActive ? "active" : ""}`}></div>
+                    </button>
+                    <div className="tablet_burger_inner">
+                        <ul className="tablet_burger_icons">
+                            <li className="search_icon"><img src={search} alt="search" /></li>
+                            <li><img className="img2" src={user} alt="myaccount" /></li>
+                            <li className="cart_icon">
+                                <img src={cart} alt="cart" />
+                                <div className="cart_itmes">3</div>
+                            </li>
+                        </ul>
+                        <ul className="tablet_burger_nav">
+                            <NavLink to="/" className="navlink_nav">
+                                <li className="isActive">
+                                    Footwear
+                                    <div className="dot"></div>    
+                                </li>
+                            </NavLink>
+                        
+                            <li className="">
+                                About us
+                                <div className=""></div> 
+                            </li>
+                            <li className="">
+                                Products
+                                <div className=""></div> 
+                            </li>
+                            <li className="">
+                                Sale
+                                <div className=""></div>     
+                            </li>
+                        </ul>
+                    </div>
+                </div>
             </div>
         </header>
      );
